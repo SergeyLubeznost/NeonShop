3c5b5e050bdc96f2c6cdca8b4e4aecd898a4f3e8
unknown <Kutsalov-dg-hub@mail.ru>
2024-05-16 20:27:25 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
 import './style.css';
-import './header/header.css';
-import './header/header-mobile.css';
-import './burger/burger.css';
-import './main-section/main-section.css';
-import './main-section/main-section-mobile.css';
+// import './header/header.css';
+// import './header/header-mobile.css';
+// import './burger/burger.css';
+// import './main-section/main-section.css';
+// import './main-section/main-section-mobile.css';
 import './slider-popular/popular-slider';
 import './slider-popular/pop-slider.css';
 import './slider-popular/pop-slider-mobile.css';
-import './ourServices/our-services.css';
-import './ourServices/our-services-mobile.css';
-import './feedback/feedback.css';
-import './feedback/feedback-mobile.css';
+// import './ourServices/our-services.css';
+// import './ourServices/our-services-mobile.css';
+// import './feedback/feedback.css';
+// import './feedback/feedback-mobile.css';
 import './feedback/feedback-send';
-import './more-neon/more-neon.css';
+// import './more-neon/more-neon.css';
 import './more-neon/more-neon';
 import './more-neon/data_products';
-import './more-neon/more-neon-mobile.css';
+// import './more-neon/more-neon-mobile.css';
 import './Our advantages/advantagesImg';
-import './Our advantages/ourAdvantages-mobille.css';
-import './Our advantages/ourAdvantages.css';
+// import './Our advantages/ourAdvantages-mobille.css';
+// import './Our advantages/ourAdvantages.css';
 import './social_block/social_block_img';
-import './social_block/social_block.css';
-import './social_block/social_block_mobile.css';
+// import './social_block/social_block.css';
+// import './social_block/social_block_mobile.css';
 import './our-project/our-project'
-import './our-project/our-project.css'
-import './our-project/our-project-mobile.css'
-import './about-us/about-us-mobile.css'
-import './about-us/about-us.css'
+// import './our-project/our-project.css'
+// import './our-project/our-project-mobile.css'
+// import './about-us/about-us-mobile.css'
+// import './about-us/about-us.css'
 import './about-us/about-us.js'
